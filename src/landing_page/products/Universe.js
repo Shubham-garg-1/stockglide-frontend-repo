3421943404233c39b363405a6dc7b9538c8a8bb4
Shubham <shubh.gargofficial@gmail.com>
2024-09-25 15:55:28 +0530

    Products page done

--- src/landing_page/products/Universe.js
+++ src/landing_page/products/Universe.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 function Universe() {
-    return ( 
-        <h1>Universe</h1>
-     );
+    return (
+        <div className="container mt-5">
+            <div className="row text-center">
+                <h1>The StockGlide Universe</h1>
+                
+                <button
+                    className="p-2 btn btn-primary mt-3 fs-5 mb-5"
+                    style={{ width: "20%", margin: "0 auto" }}
+                >
+                    Signup Now
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default Universe;
